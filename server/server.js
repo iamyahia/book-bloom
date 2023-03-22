b49b8f82be43e65a5ee9e6cb8c214d7328a2ae36
iamyahia <yahiabaiz@gmail.com>
2023-03-22 16:43:55 +0300

    config graphql schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const app = express();
 
-app.use("/graphQl", graphqlHTTP({}));
+const schema = require("./schema/schema");
+
+app.use(
+  "/graphQl",
+  graphqlHTTP({
+    schema,
+  })
+);
 
 app.listen(5000, () => console.log("server connected ..."));
