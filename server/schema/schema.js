a178faf44f95a0c4868893d2eca64877c6e3b21e
iamyahia <yahiabaiz@gmail.com>
2023-03-23 00:06:01 +0300

    setup graphql api dashboard succesfully

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const graphQl = require("graphql");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphQl;
 
+// dummy data
+const books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
@@ -13,16 +20,16 @@ const BookType = new GraphQLObjectType({
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
-  fields: () => ({
+  fields: {
     book: {
       type: BookType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        args.id;
+        return books.find((book) => book.id === args.id);
         //  code to get data from db/other source
       },
     },
-  }),
+  },
 });
 
 module.export = new GraphQLSchema({
--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
 const app = express();
 
-const schema = require("./schema/schema");
+// const schema = require("./schema/schema");
+
+// dummy data
+const books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return books.find((book) => book.id === args.id);
+        //  code to get data from db/other source
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({ query: RootQuery });
 
 app.use(
   "/graphQl",
   graphqlHTTP({
     schema,
+    graphiql: true,
   })
 );
 
